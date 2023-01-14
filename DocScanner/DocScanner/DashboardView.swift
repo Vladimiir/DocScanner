94f25bb2254dcb7671ae0e56915bbe75052b78f4
VladimirStasenko <pt.vstasenko@prosper.com>
2023-01-14 16:24:43 +0100

    added project

---
+++ DocScanner/DocScanner/DashboardView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DocScanner
+//
+//  Created by Vladimir Stasenko on 14.01.2023.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            // SV with the scanned documents
+                // click with show image mych bigger with recognised text
+            // buttons: scan, select images from 
+        }
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
---
+++ DocScanner/DocScanner/DocScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocScannerApp.swift
+//  DocScanner
+//
+//  Created by Vladimir Stasenko on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct DocScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DashboardView()
+        }
+    }
+}
